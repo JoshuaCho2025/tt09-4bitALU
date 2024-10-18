1d3cc263d7e3d1afecbe9f02e67fa624b9174490
JoshuaCho2025 <145054637+JoshuaCho2025@users.noreply.github.com>
2024-10-18 15:24:17 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,7 +9,7 @@ async def test_tt_um_Richard28277(dut):
 
     # Initialize Inputs
     dut.ui_in.value = 0
-   dut.uio_in.value = 0
+    dut.uio_in.value = 0
     dut.ena.value = 1
     dut.rst_n.value = 0
 
