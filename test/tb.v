8774343a31066399457bf0603f614bd3dbb90fec
JoshuaCho2025 <145054637+JoshuaCho2025@users.noreply.github.com>
2024-10-18 14:46:55 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -17,8 +17,8 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+   reg [3:0] a,b;
+   reg [3:0] opcode;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
@@ -36,9 +36,9 @@ module tb ();
      .VGND(VGND),
    `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
+     .ui_in  ({a,b}),    // Dedicated inputs
+     .uo_out (uo_out),   // Dedicated outputs
+      .uio_in ({4'b0000,opcode}),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
